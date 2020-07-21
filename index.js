9549aa5ffc0b68df68321a41d228c7a2c368c981
James Formica <james.formica@gmail.com>
2020-07-21 17:11:27 +1000

    this is awesome!

--- index.js
+++ index.js
@@ -1,97 +1,134 @@
 import guitars from './img/guitars.jpg'
 
+const POINTS = 10
 const RADIUS = 300
-const DIAMETER = RADIUS * 2
 const SLICE = 30
-const height = Math.abs(Math.sin(30 * Math.PI / 180) * RADIUS) + 10
+const SLICE_HEIGHT = Math.abs(Math.sin(30 * Math.PI / 180) * RADIUS) + 10
 
 const kCanvas = document.getElementById('kaleidoscope')
 const kCtx = kCanvas.getContext('2d')
 
-const imgCanvas = document.getElementById('image')
-const imgCtx = imgCanvas.getContext('2d')
-
 const patternCanvas = document.getElementById('pattern')
 const patternCtx = patternCanvas.getContext('2d')
 patternCanvas.width = RADIUS
-patternCanvas.height = height
+patternCanvas.height = SLICE_HEIGHT
 
 const invPatternCanvas = document.getElementById('invPattern')
 const invPatternCtx = invPatternCanvas.getContext('2d')
 invPatternCanvas.width = RADIUS
-invPatternCanvas.height = height
+invPatternCanvas.height = SLICE_HEIGHT
 
 const clearCanvas = (canvas, ctx) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-// Create an image element
-var img = document.createElement('img');
-
-// When the image is loaded, draw it
-
-let rotate = 0
-let x = 1200
-let y = 2000
-
-img.onload = function () {
-  imgCanvas.width = img.width
-  imgCanvas.height = img.height
-
-  const letDoIt = () => {
-    clearCanvas(imgCanvas, imgCtx)
-    clearCanvas(patternCanvas, patternCtx);
-    clearCanvas(invPatternCanvas, invPatternCtx);
-    clearCanvas(kCanvas, kCtx);
-
-
-    let degree = 345;
-    const d1 = degree - 0.5
-    const d2 = degree + SLICE + 0.5
-
-    imgCtx.save()
-    imgCtx.translate(img.width / 2, img.height / 2);
-    imgCtx.rotate(rotate * Math.PI / 180);
-    imgCtx.translate(img.width / -2, img.height / -2);
-    imgCtx.drawImage(img, 0, 0)
-    imgCtx.restore()
-
-
-    patternCtx.save()
-    patternCtx.beginPath();
-    patternCtx.moveTo(0, height / 2);
-    patternCtx.arc(0, height / 2, RADIUS, (360 - d2) * Math.PI / 180, (360 - d1) * Math.PI / 180);
-    patternCtx.lineTo(0, height / 2);
-
-    patternCtx.clip();
-    patternCtx.drawImage(imgCanvas, x, y, RADIUS, height, 0, 0, RADIUS, height)
-
-    patternCtx.restore()
-
-
-    invPatternCtx.save()
-    invPatternCtx.translate(0, height)
-    invPatternCtx.scale(1, -1);
-    invPatternCtx.drawImage(patternCanvas, 0, 0);
-    invPatternCtx.restore()
-
-    kCtx.save()
-    kCtx.translate(RADIUS, RADIUS)
-    for (let i = 0; i <= 12; i += 1) {
-      kCtx.rotate(SLICE * Math.PI / 180);
-      kCtx.drawImage(i % 2 ? patternCanvas : invPatternCanvas, 0, height / -2);
-    }
-    kCtx.restore()
-    // kCtx.drawImage(patternCanvas, 0, 0);
-    // kCtx.drawImage(patternCanvas, 300, 300 - height, 150, height, 0, 0, 150, height);
-
-    x++
-    // rotate = rotate + 0.05 % 360
-    window.requestAnimationFrame(letDoIt)
-  }
-
-  window.requestAnimationFrame(letDoIt)
+const clearCanvases = () => {
+  clearCanvas(patternCanvas, patternCtx);
+  clearCanvas(invPatternCanvas, invPatternCtx);
+  clearCanvas(kCanvas, kCtx);
 }
 
-// Specify the src to load the image
+const drawWedge = () => {
+  let degree = 345;
+  const d1 = degree - 0.5
+  const d2 = degree + SLICE + 0.2 // remove white seams
+
+  patternCtx.save()
+
+  // draw cheese wedge
+  patternCtx.beginPath();
+  patternCtx.moveTo(0, SLICE_HEIGHT / 2);
+  patternCtx.arc(0, SLICE_HEIGHT / 2, RADIUS, (360 - d2) * Math.PI / 180, (360 - d1) * Math.PI / 180);
+  patternCtx.lineTo(0, SLICE_HEIGHT / 2);
+
+  // crop image section into wedge
+  patternCtx.clip();
+  patternCtx.drawImage(img, x, y, RADIUS, SLICE_HEIGHT, 0, 0, RADIUS, SLICE_HEIGHT)
+  patternCtx.restore()
+}
+
+const drawInverseWedge = () => {
+  invPatternCtx.save()
+  invPatternCtx.translate(0, SLICE_HEIGHT)
+  invPatternCtx.scale(1, -1);
+  invPatternCtx.drawImage(patternCanvas, 0, 0);
+  invPatternCtx.restore()
+}
+
+const drawWheel = () => {
+  kCtx.save()
+  kCtx.translate(RADIUS, RADIUS)
+
+  for (let i = 0; i <= 360 / SLICE; i += 1) {
+    kCtx.rotate(SLICE * Math.PI / 180);
+    kCtx.drawImage(i % 2 ? patternCanvas : invPatternCanvas, 0, SLICE_HEIGHT / -2);
+  }
+
+  kCtx.restore()
+}
+
+const random = (min, max) => Math.floor(Math.random() * (max - min) + min)
+
+const selectPoints = (imgWidth, imgHeight) => {
+  const top = SLICE_HEIGHT
+  const left = 0
+  const right = imgWidth - RADIUS
+  const bottom = imgHeight - SLICE_HEIGHT
+
+  const points = []
+
+  for (let i = 0; i < POINTS; i += 1) {
+    points.push({ x: random(left, right), y: random(top, bottom) })
+  }
+
+  return points
+}
+
+const nextPoint = (current, destination) => {
+  if (current === destination) return 0
+  if (current > destination) return -1
+  return 1
+}
+
+let pointCounter = 1
+let x = -1
+let y = -1
+
+var img = document.createElement('img');
+img.onload = function () {
+  const points = selectPoints(img.width, img.height)
+
+  debugger
+
+  x = points[0].x
+  y = points[0].y
+
+  let destinationPoint = points[1]
+
+  const loop = () => {
+    clearCanvases()
+    drawWedge()
+    drawInverseWedge()
+    drawWheel()
+
+    const distanceX = Math.abs(x - destinationPoint.x)
+    const distanceY = Math.abs(y - destinationPoint.y)
+
+    if (distanceX === 0 && distanceY === 0) {
+      console.log('on to the next point')
+      pointCounter = (pointCounter + 1) % POINTS
+      destinationPoint = points[pointCounter]
+    } else if (distanceX === distanceY) {
+      x += nextPoint(x, destinationPoint.x)
+      y += nextPoint(y, destinationPoint.y)
+    } else if (distanceX > distanceY) {
+      x += nextPoint(x, destinationPoint.x)
+    } else {
+      y += nextPoint(y, destinationPoint.y)
+    }
+
+    window.requestAnimationFrame(loop)
+  }
+  window.requestAnimationFrame(loop)
+}
 img.src = guitars;
