360a26e04f9d14657dd232bc860807a4d819a498
james.formica <james.formica@gmail.com>
2021-07-17 14:42:27 +1000

    Updates

--- muetry.eef18777.js
+++ muetry.eef18777.js
@@ -338,7 +338,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61307" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55936" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
